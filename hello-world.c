4a2d69fdd76eb14527e188560d62f840337e03e4
turkmencodes <108829807+turkmencodes@users.noreply.github.com>
2022-07-17 23:54:10 +0300

    Create hello-world.c

---
+++ hello-world.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+// Bu kitaphan 
+// This is a library
+
+
+int main() // Bu Eesasy bolumi, ol programma degisli in esasy kodlary oz icine alyar  = This is main part, it keeps main codes for program
+{
+    printf("Hello World"); // Bu printf funkisiyasy, ol bize programmadan helyan zatlary by cykarmaga komek etyar = This a printf function, it helps us to show outputs from the program 
+    
+    return 0; // Bu bizin kodymyzyn gutarandygyny ayytyar sistema == it means that ending our codes...
+    
+    }
