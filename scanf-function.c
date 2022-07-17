c2ae9b437c88da9e8481a4a95f1b7028065b1bc5
turkmencodes <108829807+turkmencodes@users.noreply.github.com>
2022-07-18 00:02:53 +0300

    Create scanf-function.c

---
+++ scanf-function.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+  int a; // Bu yglan etmek= this is a declaration
+  
+  printf("Enter the number");
+  
+  scanf("%d", &a); // Bu scanf funkisyasy ol byzyn yan zadymy ygylan ydilen zada birikdiryar = this is the scanf function it links our input to the declaration
+  
+  printf("your number is %d", a);
+  
+  return 0;
+}
